3c55727c6beb278a2e373177dce99cdbd703bd1b
SPD504 <spham23@lsu.edu>
2025-01-29 21:39:58 -0600

    Example2

--- sketch.js
+++ sketch.js
@@ -3,26 +3,19 @@ function setup() {
 }
 
 function draw() {
-  background('black');
+  colorMode(RGB, 255, 255, 255, 1);
+  background(255);
 
-  fill(255, 255, 0);
+  blendMode(BLEND);
   noStroke();
-  circle(200, 200, 200);
 
-  fill(0);
-  triangle(200, 200, 100, 140, 100, 260);
 
-  fill(255, 0, 0);
-  noStroke();
-  arc(440, 150, 180, 100, 90, 0);
-  rect(350, 150, 180, 140);
+  fill(255, 182, 193, 0.7);
+  circle(300, 100, 200);
 
-  fill(255);
-  circle(400, 180, 40);
-  circle(485, 180, 40);
+  fill(173, 216, 230, 0.7);
+  circle(230, 230, 200);
 
-  fill(0, 0, 255);
-  circle(400, 180, 25);
-  circle(485, 180, 25);
-
+  fill(144, 238, 144, 0.7);
+  circle(360, 230, 200);
 }
